43eb2d3fb57b34e5bfcda3994e425b8a8fe7dd3a
KartikGupta26 <kartikgupta867@gmail.com>
2022-05-20 15:45:28 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section className='section about-section'>
+            <h1 className='section-title'>about page</h1>
+            <p style={{fontSize:"20px"}}>
+                Most commonly, cocktails are either a combination of spirits, or one or more spirits mixed with other ingredients such as tonic water, fruit juice, flavored syrup, or cream. Cocktails vary widely across regions of the world, and many websites publish both original recipes and their own interpretations of older and more famous cocktails
+
+            </p>
+        </section>
+    )
+}
+
+export default About
